48a985d36d6e4c2b87a5d4f2ca09f0b39f08f292
DESKTOP-0P18NTF\frameout <kyebn@frameout.co.kr>
2025-11-26 18:22:23 +0900

    [251124] 스크롤 gsap 수정

--- src/page/Gate/section/Skill.jsx
+++ src/page/Gate/section/Skill.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -10,74 +10,78 @@ import cn from "classnames";
 gsap.registerPlugin(ScrollTrigger);
 
 const Skill = forwardRef((props, ref) => {
-  const active = 0;
-
   const listRef = useRef(null);
   const wrapRef = useRef(null);
+  const [active, setActive] = useState(0);
+
   // const tl = useRef(null);
 
   useGSAP(() => {
-    const length = props.data.length; // 예: 5
-    const step = 1 / length;
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: wrapRef.current,
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-        pin: listRef.current,
+    const steps = props.data.length - 1; // 예: 5
+    const stepDeg = 10; // 단계별 회전각
+
+    ScrollTrigger.create({
+      trigger: wrapRef.current,
+      start: "top top",
+      end: "bottom-=10% bottom",
+      onUpdate: (self) => {
+        const progress = self.progress;
+        const currentStep = Math.floor(progress * steps);
+        gsap.to(listRef.current, {
+          rotate: -stepDeg * currentStep,
+          duration: 0.4,
+        });
+
+        setActive(currentStep);
       },
     });
-
-    props.data.forEach((_, index) => {
-      tl.to(listRef.current, { rotate: -10 * length }, step * index);
-    });
-    // .to(listRef.current, { rotate: -10 });
   });
 
   console.log(props.data, props.data.length);
   return (
     <section className={cn("skill", props.className)} ref={ref}>
       <div className="skill__wrap" ref={wrapRef}>
-        <div className="skill-list" ref={listRef}>
-          <strong className="skill-list__tit">Skill</strong>
-          <ul className="skill-list__list">
-            {props.data.map((item, num) => {
-              return (
-                <li className="skill-list__item" key={num}>
-                  <div
-                    className={cn("skill-item", {
-                      "skill-item--active": num === active,
-                    })}
-                  >
-                    <SparkleIco
-                      className="skill-item__ico"
-                      color={num == active ? "0980e3" : "222222"}
-                    />
-                    <span className="skill-item__num">
-                      {num + 1 < 10 && 0}
-                      {num + 1}
-                    </span>
-                    <div className="skill-item__box">
-                      <div className="skill-item__img-cont">
-                        <img
-                          src={item.img}
-                          alt={`${item.name} 로고`}
-                          className="skill-item__img"
-                        />
-                      </div>
-                      <div className="skill-item__txt">
-                        {item.sub && (
-                          <span className="skill-item__sub">{item.sub}</span>
-                        )}
-                        <span className="skill-item__tit">{item.name}</span>
+        <div className="skill__sticky">
+          <div className="skill-list">
+            <strong className="skill-list__tit">Skill</strong>
+            <ul className="skill-list__list" ref={listRef}>
+              {props.data.map((item, num) => {
+                return (
+                  <li className="skill-list__item" key={num}>
+                    <div
+                      className={cn("skill-item", {
+                        "skill-item--active": num === active,
+                      })}
+                    >
+                      <SparkleIco
+                        className="skill-item__ico"
+                        color={num == active ? "0980e3" : "222222"}
+                      />
+                      <span className="skill-item__num">
+                        {num + 1 < 10 && 0}
+                        {num + 1}
+                      </span>
+                      <div className="skill-item__box">
+                        <div className="skill-item__img-cont">
+                          <img
+                            src={item.img}
+                            alt={`${item.name} 로고`}
+                            className="skill-item__img"
+                          />
+                        </div>
+                        <div className="skill-item__txt">
+                          {item.sub && (
+                            <span className="skill-item__sub">{item.sub}</span>
+                          )}
+                          <span className="skill-item__tit">{item.name}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </section>
